1d706e6d55cb7111d30540f87b53d82aa5996186
Charles Fischer <cfischer2247@gmail.com>
2025-07-17 11:25:49 -0600

    added writer.c

---
+++ finder-app/writer.c
@@ -0,0 +1,32 @@
+#include <syslog.h>
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	openlog("finderapp", LOG_PID, LOG_USER);
+	// verify there are two addional args file and string besides the filename
+	if (argc != 3) {
+		syslog(LOG_INFO, "Two arguments required");
+	}
+
+	char *writefile = argv[1];
+	char *writestr = argv[2];
+	//open file writer
+	FILE *writer = fopen(writefile, "w");
+	if (writer == NULL) {
+		syslog(LOG_ERR, "Error opening file");
+	}
+	//write string to file
+	if (fprintf(writer, "%s", writestr) < 0){
+		syslog(LOG_ERR, "Error writing to file");
+		fclose(writer);
+	}
+
+	syslog(LOG_DEBUG, "Writing %s to %s", writefile, writestr);
+
+	//clean
+	fclose(writer);
+	closelog();
+}
