e40ae373db526dce1e6413b1cafeddbeef1e3754
Charles Fischer <cfischer2247@gmail.com>
2025-07-17 12:19:29 -0600

    added exit code

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	openlog("finderapp", LOG_PID, LOG_USER);
+     	openlog("finderapp", LOG_PID, LOG_USER);
 	// verify there are two addional args file and string besides the filename
 	if (argc != 3) {
 		syslog(LOG_INFO, "Two arguments required");
+		exit(1);
 	}
 
 	char *writefile = argv[1];
@@ -17,11 +18,13 @@ int main(int argc, char *argv[]){
 	FILE *writer = fopen(writefile, "w");
 	if (writer == NULL) {
 		syslog(LOG_ERR, "Error opening file");
+		exit(1);
 	}
 	//write string to file
 	if (fprintf(writer, "%s", writestr) < 0){
 		syslog(LOG_ERR, "Error writing to file");
 		fclose(writer);
+		exit(1);
 	}
 
 	syslog(LOG_DEBUG, "Writing %s to %s", writefile, writestr);
